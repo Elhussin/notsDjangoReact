508efbbc905150384d9bd85777a50e40b8dd0cb4
Elhussin <hasin3112@gmail.com>
2025-02-17 22:39:12 +0300

    add rest passord commpent

---
+++ react-app/src/components/users/PasswordResetRequest .js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const PasswordResetRequest =()=>{
+
+    const [email, setEmail] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState('');
+    const [success, setSuccess] = useState(false);
+
+
+}
